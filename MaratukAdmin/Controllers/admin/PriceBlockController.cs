b86542962e05fc27a8c4fd9841788da2f1961796
sevakhayriyan <99863191+sevakhayriyan@users.noreply.github.com>
2023-07-10 23:11:46 +0400

    update

--- MaratukAdmin/Controllers/admin/PriceBlockController.cs
+++ MaratukAdmin/Controllers/admin/PriceBlockController.cs
@@ -75,33 +75,12 @@ namespace MaratukAdmin.Controllers.admin
             }
         }
 
-        [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status403Forbidden)]
-        public async Task<ActionResult> CreatTestePriceBlockAsync([FromBody] AddPriceBlockRequest priceBlockRequest)
-        {
-            try
-            {
-                //call manager
-                var result = await _priceBlockManager.AddPriceBlockAsync(priceBlockRequest);
-
-                return Ok(result);
-            }
-            catch (ArgumentException ex)
-            {
-                return Forbid(ex.Message);
-            }
-            catch (Exception)
-            {
-                return BadRequest("Something went wrong");
-            }
-        }
 
 
 
         [HttpPut]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public async Task<IActionResult> UpdateFlightAsync([FromBody] UpdatePriceBlockRequest priceBlockRequest)
+        public async Task<IActionResult> UpdatePriceBlockAsync([FromBody] UpdatePriceBlockRequest priceBlockRequest)
         {
             var result = await _priceBlockManager.UpdatePriceBlockAsync(priceBlockRequest);
             return Ok(result);
@@ -110,7 +89,7 @@ namespace MaratukAdmin.Controllers.admin
         [HttpDelete("{id:int}")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public async Task<IActionResult> DeleteFlightAsync(int id)
+        public async Task<IActionResult> DeletePriceBlockAsync(int id)
         {
             var result = await _priceBlockManager.DeletePriceBlockAsync(id);
             return Ok(result);
