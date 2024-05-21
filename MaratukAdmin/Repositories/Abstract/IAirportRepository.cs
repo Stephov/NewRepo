22367fce72de3157468cad7bdd884b89fdcb80e7
Step <hovakimyan.stepan@gmail.com>
2024-05-22 00:28:38 +0400

    GetAirportNameByCodeAsync added AirportName added

---
+++ MaratukAdmin/Repositories/Abstract/IAirportRepository.cs
@@ -0,0 +1,9 @@
+﻿using MaratukAdmin.Entities.Global;
+
+namespace MaratukAdmin.Repositories.Abstract
+{
+    public interface IAirportRepository
+    {
+        Task<Airport?> GetAirportNameByCodeAsync(string code);
+    }
+}
